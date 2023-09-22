a51dded384fa233c7f3ac105531e8dad193bc56c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-09-22 20:04:15 +0200

    wait this works?

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,17 @@ type RecordMap = {
     a: ["hi", 1],
     b: [2, "jo"]
 };
-type UnionRecord<K extends keyof RecordMap = keyof RecordMap> = { [P in K]: [RecordMap[P]["0"], RecordMap[P]["1"]] }[K];
-type UnionRecord2<K extends keyof RecordMap = keyof RecordMap> = { [P in K]: RecordMap[P] }[K];
 
-const a: UnionRecord<"a"> = ["hi", 1];
-const a1: UnionRecord2<"a"> = a;
+type UnionRecordBoth<K extends keyof RecordMap = keyof RecordMap> = { [P in K]: [RecordMap[P]["0"], RecordMap[P]["1"]] }[K];
+type UnionRecord<K extends keyof RecordMap = keyof RecordMap> = { [P in K]: RecordMap[P] }[K];
+type UnionRecord0<K extends keyof RecordMap = keyof RecordMap> = { [P in K]: RecordMap[P]["0"] }[K];
+type UnionRecord1<K extends keyof RecordMap = keyof RecordMap> = { [P in K]: RecordMap[P]["1"] }[K];
 
+const aboth: UnionRecordBoth<"a"|"b"> = ["hi", 1];
+const a: UnionRecord<"a"|"b"> = aboth;
+
+const a0: UnionRecord0<"a"|"b"> = "hi";
+const a1: UnionRecord1<"a"|"b"> = 1;
+
+const ba: UnionRecord<"a"|"b"> = [a0, a1];
+const baboth: UnionRecordBoth<"a"|"b"> = [a0, a1];
